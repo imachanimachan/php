8d28d52ea1e9af92bfe519b349c31da1b3dbbd23
myon <iena0926@outlook.jp>
2025-02-17 16:39:42 +0900

    コミットします

--- src/php01/config/result.php
+++
@@ -1,8 +0,0 @@
-<?php
-$name= htmlspecialchars($_POST['name'], ENT_QUOTES);
-$shouhin = htmlspecialchars($_POST['shouhin'], ENT_QUOTES);
-$kazu = htmlspecialchars($_POST['kazu'], ENT_QUOTES);
-
-print "私の名前は" . $name . "です"."<br/>";
-print "ご希望の商品は" . $shouhin . "です" . "<br/>";
-print "注文数は" . $kazu . "です"; 
